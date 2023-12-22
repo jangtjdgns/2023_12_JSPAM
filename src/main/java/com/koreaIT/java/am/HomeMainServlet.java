d2baaecdaa9e6ce730659fdc30fc35f476d13605
jangtjdgns <jangtjdgns0208@gmail.com>
2023-12-22 21:11:59 +0900

    url로 구구단 입력받기 [단, 색상, 제한 설정]

--- src/main/java/com/koreaIT/java/am/HomeMainServlet.java
+++ src/main/java/com/koreaIT/java/am/HomeMainServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main")
+@WebServlet("/home/printDan")
 public class HomeMainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -17,13 +17,13 @@ public class HomeMainServlet extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8;");
 		
 		int inputDan = Integer.parseInt(request.getParameter("dan"));
-		String color = request.getParameter("color");
-		int limit = Integer.parseInt(request.getParameter("limit"));
+		String inputColor = request.getParameter("color");
+		int inputLimit = Integer.parseInt(request.getParameter("limit"));
 		
-		response.getWriter().append(String.format("<div style='color: %s;'>== %d단 ==</div>", color, inputDan));
+		response.getWriter().append(String.format("<div style=\"color: %s;\">== %d단 ==</div>", inputColor, inputDan));
 		
-		for(int i=1; i<=limit; i++) {
-			response.getWriter().append(String.format("<div style='color: %s;'>%d * %d = %d</div>", color, inputDan, i, inputDan * i));
+		for(int i=1; i<=inputLimit; i++) {
+			response.getWriter().append(String.format("<div style=\"color: %s;\">%d * %d = %d</div>", inputColor, inputDan, i, inputDan * i));
 		}
 	}
 }
